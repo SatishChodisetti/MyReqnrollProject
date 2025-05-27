bb4ad285da1f1cd285ca10f2fc5501c241c11843
Satish Chodisetti <satishnani1792@gmail.com>
2025-05-27 18:40:09 +0530

    Committing check webhooks

--- StepDefinitions/APIStepDefinitions.cs
+++ StepDefinitions/APIStepDefinitions.cs
@@ -15,34 +15,28 @@ public sealed class APIStepDefinitions
     [Then("the response should contain a list of {string}")]
     public void ThenTheResponseShouldContainAListOf(string s)
     {
-        Console.WriteLine("API Test 1");
-
         var json = JObject.Parse(_response.Content);
-            Assert.That(json.ContainsKey("data"), Is.True,"Response does not contain 'data'");
-
-            var data = json["data"];
-            Assert.That(data.Type == JTokenType.Array, "Expected 'data' to be an array");
-            Console.WriteLine($"Resource data count: {data.Count()}");
+        Assert.That(json.ContainsKey("data"), Is.True, "Response does not contain 'data'");
+        var data = json["data"];
+        Assert.That(data.Type == JTokenType.Array, "Expected 'data' to be an array");
+        Console.WriteLine($"Resource data count: {data.Count()}");
     }
 
     [Then("the response status code should be {int}")]
     public void ThenTheResponseStatusCodeShouldBe(int i)
     {
-        Console.WriteLine("API Test 2");
         Assert.That((int)_response.StatusCode, Is.EqualTo(200), "Expected status code 200");
     }
 
     [When("I send a GET request to the endpoint")]
     public void WhenISendAGETRequestToTheEndpoint()
     {
-        Console.WriteLine("API Test 3");
         _response = _client.Execute(_request);
     }
 
     [Given("I have the API endpoint for {string} with page {string} and per_page {string}")]
     public void GivenIHaveTheAPIEndpointForWithPageAndPer_page(string s, string s2, string s3)
     {
-        Console.WriteLine("API Test 4");
         _client = new RestClient("https://reqres.in/");
         _request = new RestRequest("api/users", Method.Get);
         _request.AddParameter("page", 1).AddParameter("per_page", 2).AddHeader("x-api-key", "reqres-free-v1");
